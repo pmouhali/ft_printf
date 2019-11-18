9bee24fd95aa7878aa9b44999b40f0abdc4c7f52
Paloma MOUHALI <pmouhali@e1r5p19.42.fr>
2019-11-18 19:57:30 +0100

    empty tests to complete

---
+++ minfieldsize_simple.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* taille de champ minimale, nombre optionnel avant la conversion */
+/* test pertinents : value sup/inf a strlen de conv, 0, negatives */
+
+int	main(void)
+{
+	
+}
