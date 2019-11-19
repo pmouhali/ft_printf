da5756e6d572b3219dae61af8fff6646182b874b
Paloma MOUHALI <pmouhali@e1r4p19.42.fr>
2019-11-19 12:07:29 +0100

    all conversion for flag simpple -/0

--- flag_-_simple.c
+++ flag_-_simple.c
@@ -29,4 +29,66 @@ int	main(void)
 	/* printf("%-0s\n", s); warning says :  */
 	/* "flag '0' is ignored when flag '-' is present */
 	/* that's why -00 */
+
+	/* d conversion : */
+
+	printf("%-d\n", i);
+	printf("%-00d\n", i); 
+	printf("%-10d\n", i);
+	printf("%-1d\n", i); 
+	printf("%-2d\n", i);
+	printf("%-3d\n", i);
+	printf("%-4d\n", i);
+
+	/* u conversion : */
+
+	printf("%-u\n", i);
+	printf("%-00u\n", i);
+	printf("%-10u\n", i);
+	printf("%-1u\n", i);
+	printf("%-2u\n", i);
+	printf("%-3u\n", i);
+	printf("%-4u\n", i);
+
+	/* x conversion : */
+
+	printf("%-x\n", i);
+	printf("%-00x\n", i);
+	printf("%-10x\n", i);
+	printf("%-1x\n", i);
+	printf("%-2x\n", i);
+	printf("%-3x\n", i);
+	printf("%-4x\n", i);
+
+	/* X conversion : */
+
+	printf("%-x\n", i);
+	printf("%-00x\n", i);
+	printf("%-10x\n", i);
+	printf("%-1x\n", i);
+	printf("%-2x\n", i);
+	printf("%-3x\n", i);
+	printf("%-4x\n", i);
+
+	/* c conversion : */
+
+	printf("%-c\n", i);
+	printf("%-00c\n", i);
+	printf("%-10c\n", i);
+	printf("%-1c\n", i);
+	printf("%-2c\n", i);
+	printf("%-3c\n", i);
+	printf("%-4c\n", i);
+
+	/* p conversion : */
+
+	printf("%-p\n", s);
+	printf("%-00p\n", s);
+	printf("%-10p\n", s);
+	printf("%-11p\n", s);
+	printf("%-12p\n", s);
+	printf("%-13p\n", s);
+	printf("%-14p\n", s);
+
+	/* Meme resultat partout */
 }
