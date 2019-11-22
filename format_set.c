05c7e2b5eaaee1f3a039c9fee8b84b365ca8ae7f
Paloma MOUHALI <pmouhali@e1r9p17.42.fr>
2019-11-22 18:04:32 +0100

    start coding format_set

---
+++ format_set.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   format_set.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/22 17:45:09 by pmouhali          #+#    #+#             */
+/*   Updated: 2019/11/22 18:03:52 by pmouhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/* retourne l'index du dernier charactere lu				*/
+/* s'arrete sur le premier char non inclus dans les flags authorises	*/
+
+int	format_set(const char *f, va_list l, s_format *fmt)
+{
+	unsigned int i;
+
+	i = 1;
+	while (ft_index(A_FLAGS, f[i]) != -1 || ft_isdigit(f[i]))
+		i++;
+	return (i);
+} 
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 12:12:22 by pmouhali          #+#    #+#             */
-/*   Updated: 2019/11/21 14:15:53 by pmouhali         ###   ########.fr       */
+/*   Updated: 2019/11/22 18:03:26 by pmouhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+# define A_FLAGS "0-.*"
+
 int	ft_printf(const char *f, ...);
 char	*format_string(const char *f, va_list l);
 int     format_check(const char *f, va_list l);
@@ -25,5 +27,7 @@ char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 int	ft_strlen(const char *s);
 void	ft_putstr(const char *s);
+int     ft_index(char const *s, char c);
+int	ft_isdigit(int c);
 
 #endif
